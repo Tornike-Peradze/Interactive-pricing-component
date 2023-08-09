87cc9fbea420c38adf17a68e3f88b12d8f1142e4
Tornike-Peradze <t_peradze@cu.edu.ge>
2023-08-09 21:58:14 +0400

    js starter point

---
+++ main.js
@@ -0,0 +1,10 @@
+let scrollBar = document.getElementById("input");
+let payment = document.querySelector(".monthly-payment");
+let switchToMonthYear = document.querySelector(".month");
+let checkBox = document.getElementById("checkbox");
+
+let actualPayement = 16;
+
+scrollBar.onclick = functiom() {
+  
+};
